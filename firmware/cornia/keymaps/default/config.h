53a7056cc96fd9d18baf94c7821d65c732a4130c
Vaarai <joshwolfcarius@gmail.com>
2024-08-30 20:49:33 +0200

    [FW] Add FW v1 (not tested)

---
+++ firmware/cornia/keymaps/default/config.h
@@ -0,0 +1,36 @@
+/* Copyright 2024 Vaarai
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#ifdef OLED_ENABLE
+#   define SPLIT_LAYER_STATE_ENABLE
+#   define SPLIT_LED_STATE_ENABLE
+#   define SPLIT_MODS_ENABLE
+#   define SPLIT_OLED_ENABLE
+#   define OLED_TIMEOUT 60000
+#endif
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
---
+++ firmware/cornia/keymaps/default/custom_keycodes.h
@@ -0,0 +1,27 @@
+/* Copyright 2024 Vaarai
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/* Custom Keycodes (CK_xxx) */
+#define CK_LPAR LSFT(KC_9)
+#define CK_RPAR LSFT(KC_0)
+#define CK_LCBR LSFT(KC_LBRC)
+#define CK_RCBR LSFT(KC_RBRC)
+#define CK_QMRK LSFT(KC_SLSH)
+#define CK_SSHT LSG(KC_S)
+#define CK_UNSC LSFT(KC_MINS)
+
